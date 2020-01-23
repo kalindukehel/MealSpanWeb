c0eec62dda4abcd4f8425e38e3f45f260a296f54
Kalindu <59674165+kalindu-k@users.noreply.github.com>
2020-01-23 17:53:40 -0500

    Add login function and display balance

--- login.py
+++ login.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 
 
 def authenticate(username,password):
+    basic = ''
+    flex = ''
     with requests.session() as s:
         page = s.get('http://onlineservices.hospitality.uoguelph.ca/')
         soup = BeautifulSoup(page.text,'html5lib')
@@ -12,9 +14,15 @@ def authenticate(username,password):
         OAM_REQ = soup.find('input',{'name':'OAM_REQ'})['value']
         payload = {'username':username,'password':password,'request_id':request_id,'OAM_REQ':OAM_REQ}
         logpage = s.post('https://sso2.identity.uoguelph.ca/oam/server/auth_cred_submit',data=payload)
-        print(logpage.text)
-    if(logpage.url == 'https://onlineservices.hospitality.uoguelph.ca/student/studenthome.cshtml'):
-        return('Valid')
-    else:
-        return('False')
-    return('a')
+        print(logpage.url)
+        statuscode = 1
+        if(logpage.url == 'https://onlineservices.hospitality.uoguelph.ca/student/studenthome.cshtml'):
+            statuscode = 0
+            balpage = s.get('https://onlineservices.hospitality.uoguelph.ca/secure/onlineinquiry.cshtml')
+            balsoup = BeautifulSoup(balpage.text,'html5lib')
+            basic = balsoup.find_all('td',{'align':'left'})[3].text
+            flex = balsoup.find_all('td',{'align':'left'})[5].text
+        else:
+            statuscode = 1
+
+    return(basic,flex,statuscode)
--- webapp.py
+++ webapp.py
@@ -17,8 +17,12 @@ def form_post():
     username = request.form['username']
     password = request.form['password']
     #authenticate(username,password)
-    bal = login.authenticate(username,password)
-    return render_template('balance.html',balance=bal)
+    basic,flex,statuscode = login.authenticate(username,password)
+    print(statuscode)
+    if(statuscode == 1):
+        return render_template('balance.html',basic= '0',flex='0')
+    else:
+        return render_template('balance.html',basic = basic, flex=flex)
 
 
 if __name__ == '__main__':  #if run with python $ python webapp.py     then it runs with debug mode on
