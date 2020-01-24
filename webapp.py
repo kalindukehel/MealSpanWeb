52f580b63e93850dbd2b71bc244718d7a75d0159
Kalindu <59674165+kalindu-k@users.noreply.github.com>
2020-01-23 23:23:28 -0500

    Add exclude weekends and CSS files

--- webapp.py
+++ webapp.py
@@ -12,18 +12,22 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/",methods=['POST'])
+@app.route("/balance",methods=['GET','POST'])
 def form_post():
-    username = request.form['username']
-    password = request.form['password']
-    date = request.form['date']
-    #authenticate(username,password)
-    basic,flex,statuscode = login.authenticate(username,password)
-    spending = login.calculatespending(basic,flex,date)
-    if(statuscode == 1):
-        return render_template('balance.html',basic= '0',flex='0',spend=0)
-    else:
-        return render_template('balance.html',basic = basic, flex=flex,spend=spending)
+    if(request.method == 'GET'):
+        back = url_for('home')
+        return ("<a href="+ back+'>'+"a</a>")
+    elif(request.method == 'POST'):
+        username = request.form['username']
+        password = request.form['password']
+        date = request.form['date']
+        #authenticate(username,password)
+        basic,flex,statuscode = login.authenticate(username,password)
+        if(statuscode == 1):
+            return render_template('balance.html',basic= '0',flex='0',spend=0)
+        else:
+            spending = login.calculatespending(basic,flex,date)
+            return render_template('balance.html',basic = basic, flex=flex,spend=spending)
 
 
 if __name__ == '__main__':  #if run with python $ python webapp.py     then it runs with debug mode on
